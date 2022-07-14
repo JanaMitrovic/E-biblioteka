35128ae2aa332cab4986b1cc5ea25db02d271912
AnaMilic <76072963+AnaMilic@users.noreply.github.com>
2022-07-14 22:56:18 +0200

    Dodate rute

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,5 +1,6 @@
 import { NgModule } from '@angular/core';
 import { PreloadAllModules, RouterModule, Routes } from '@angular/router';
+import { AuthGuard } from './auth/auth.guard';
 
 const routes: Routes = [
   // {
@@ -9,13 +10,33 @@ const routes: Routes = [
   {
     path: 'books',
     loadChildren: () => import('./books/books.module').then(m => m.BooksPageModule),
-    
+    canLoad: [AuthGuard]
+  },
+  {
+    path: 'log-in',
+    loadChildren: () => import('./auth/log-in/log-in.module').then( m => m.LogInPageModule)
+  },
+  {
+    path: 'register',
+    loadChildren: () => import('./auth/register/register.module').then( m => m.RegisterPageModule)
+  },
+  {
+    path: 'borrowed',
+    loadChildren: () => import('./borrowed/borrowed.module').then( m => m.BorrowedPageModule)
+  },
+  {
+    path: 'add-new',
+    loadChildren: () => import('./add-new/add-new.module').then( m => m.AddNewPageModule)
   },
   {
     path: '',
     redirectTo: 'books',
     pathMatch: 'full'
+  },  {
+    path: 'borrowed',
+    loadChildren: () => import('./borrowed/borrowed.module').then( m => m.BorrowedPageModule)
   },
+
   
   
 ];
